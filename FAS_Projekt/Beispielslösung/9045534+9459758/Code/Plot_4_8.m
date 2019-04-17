0b6066979fae427ec1d2ec1b32bf85b03a501e6c
Melanie Schneider <Mel@linux-2.fritz.box>
2019-04-17 15:36:30 +0200

    Renamed and doubled files

--- FAS_Projekt/Beispielslösung/9045534+9459758/Code/Plot_4_8.m
+++ FAS_Projekt/Beispielslösung/9045534+9459758/Code/Plot_4_8.m
@@ -1,6 +1,6 @@
-function Plot_4_8(plotInstance, gravity, lv, lh, lEG, lSG, is, m, theta)
+function Plot_4_8(plotInstance, g, lv, lh, lEG, lSG, is, m, theta)
 deltaControlled = 0;
-g = gravity;
+%g = gravity;
 
 EG = [max(-lEG, -lSG*0.9) 0 max(lEG, -lSG*0.9)];
 SG = lSG;  % SG must be greater than min(EG)!
@@ -10,8 +10,8 @@ CvStern = convGradToCStern(EG, 0, g, ChStern);
 vmax = int32(sqrt((lv+lh)/max(EG)) + 5); % charakteristische Geschwindigkeit
 vmax = vmax + (5 - mod(vmax,5)); % Runden auf 5er Schritte
 % vkrit = 20;
-deltaH = 1;
-deltaH_array = deltaH * ones(1,int32(vmax));
+delta_h = 1;
+delta_h_array = delta_h * ones(1,int32(vmax));
 v = 1:vmax;
 max_limit_y = 0;
 
@@ -19,8 +19,8 @@ colors = [[0 0 0.5], [0.8 0.4 0], [0.5 0 0]];
 cut_axis = (0 == 1);
 hold(plotInstance,'off');
 for k = 1:size(CvStern,2)
-    [~, ~, dpsi_val] = ESM_Run(10, 0.001, deltaH_array, 1, v, CvStern(k), ChStern, deltaControlled, lv, lh, is, m, theta, g);
-    yVals = dpsi_val/(deltaH/is);
+    [~, ~, dpsi_val] = ESM_Run(10, 0.001, delta_h_array, 1, v, CvStern(k), ChStern, deltaControlled, lv, lh, is, m, theta, g);
+    yVals = dpsi_val/(delta_h/is);
     max_limit_y = max(max(yVals), max_limit_y);
     if k > 1
         plot(plotInstance, v, yVals, 'LineWidth', 2, 'Color', colors((k-1)*3+1:k*3));
